ead8873e0bec8c55f77c9f540229e216b504298d
G-Breeze <1113823696@qq.com>
2021-02-09 00:14:08 +0800

    update classes: Level.java LevelType.java PlayerLib.java PlayerListener.java BlockListener.java

--- src/main/java/cn/frabys/main/Frabys.java
+++ src/main/java/cn/frabys/main/Frabys.java
@@ -1,4 +1,16 @@
 package cn.frabys.main;
 
+
+import java.util.HashMap;
+import java.util.logging.Level;
+
 public class Frabys {
+    private final PlayerListener playerListener = new PlayerListener();
+    private final BlockListener blockListener = new BlockListener();
+    private final HashMap<String, Level> hmp = new HashMap<String, Level>();
+
+
+
+    public static void main(String[] args) {
+    }
 }
