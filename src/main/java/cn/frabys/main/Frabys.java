ea142c8acddda4b038cb3a479894328011daf344
G-Breeze <1113823696@qq.com>
2021-02-10 22:52:49 +0800

    update classes: Level.java Frabys.java

--- src/main/java/cn/frabys/main/Frabys.java
+++ src/main/java/cn/frabys/main/Frabys.java
@@ -1,7 +1,18 @@
 package cn.frabys.main;
 
 
+import cn.frabys.main.basic.Level;
+import cn.frabys.main.levels.LevelType;
+
 public class Frabys {
-
-
+    public static void main(String[] args) {
+        Level test = new Level(0,"test","BLACK",10) {
+            @Override
+            public void addEx(double ex0) {
+                super.addEx(ex0);
+            }
+        };
+        test.addEx(10);
+        System.out.println(test.getLevel());
+    }
 }
--- src/main/java/cn/frabys/main/basic/Level.java
+++ src/main/java/cn/frabys/main/basic/Level.java
@@ -12,13 +12,68 @@ public abstract class Level {
 
     double ex;
 
-    double nowEx;
+    double nowEx = 0;
 
-    public Level(int level, String name, LevelType type, double ex, double nowEx) {
+    public Level(int level, String name, LevelType type, double ex) {
         this.level = level;
         this.name = name;
         this.type = type;
         this.ex = ex;
         this.nowEx = nowEx;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LevelType getType() {
+        return type;
+    }
+
+    public void setType(LevelType type) {
+        this.type = type;
+    }
+
+    public double getEx() {
+        return ex;
+    }
+
+    public void setEx(double ex) {
+        this.ex = ex;
+    }
+
+    public double getNowEx() {
+        return nowEx;
+    }
+
+    public void setNowEx(double nowEx) {
+        this.nowEx = nowEx;
+    }
+
+    public void addEx(double ex0){  //TODO
+        this.nowEx +=ex0;
+        if(this.nowEx >= this.ex){
+            this.nowEx -= this.ex;
+            level ++;
+            ex*= 10;
+            if(this.nowEx >= this.ex) {
+                double muchEx = this.nowEx -= this.ex;
+                level ++;
+                ex*= 10;
+                addEx(muchEx);
+            }
+        }
     }
 }
