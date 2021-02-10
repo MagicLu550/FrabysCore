93097a83dbc2d8318e8d3c2216e00b46e23a2685
magiclu550 <843983728@qq.com>
2021-02-10 22:25:13 +0800

    Merge branch 'master' of /Users/luchangcun/Downloads/frabys with conflicts.

--- src/main/java/cn/frabys/main/basic/Level.java
+++ src/main/java/cn/frabys/main/basic/Level.java
@@ -13,12 +13,4 @@ public abstract class Level {
     double ex;
 
     double nowEx;
-
-    public Level(int level, String name, LevelType type, double ex, double nowEx) {
-        this.level = level;
-        this.name = name;
-        this.type = type;
-        this.ex = ex;
-        this.nowEx = nowEx;
-    }
 }
